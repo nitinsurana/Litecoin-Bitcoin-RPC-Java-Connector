e12875463c0f0aa5d74b5e85e7534ee77fc6e89b
Romantsov <dm.romantsov@gmail.com>
2015-06-23 18:19:13 +0300

    Implementation of listening wallet and alert event.

---
+++ src/main/java/com/nitinsurana/bitcoinlitecoin/rpcconnector/events/BitcoinDListener.java
@@ -0,0 +1,68 @@
+package com.nitinsurana.bitcoinlitecoin.rpcconnector.events;
+
+import org.apache.log4j.Logger;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Observable;
+
+public class BitcoinDListener extends Observable implements Runnable {
+	public static final Logger LOG = Logger.getLogger(BitcoinDListener.class);
+	private final ServerSocket server;
+	private int port;
+
+	public BitcoinDListener(int port) throws IOException {
+		server = new ServerSocket(port);
+		this.port = port;
+	}
+	
+	public BitcoinDListener(ServerSocket server) throws IOException {
+		this.server = server;
+		this.port = server.getLocalPort();
+	}
+
+	@Override
+	public void run() {
+		LOG.info("Thread " + Thread.currentThread().getName()
+				+ "started lintening on " + port);
+		while (!Thread.currentThread().isInterrupted()) {
+			Socket connection = null;
+			try {
+				connection = server.accept();
+			} catch (IOException e1) {
+				e1.printStackTrace();
+				break;
+			}
+			try {
+				BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+			    String line;
+			    if ((line = reader.readLine()) != null) {
+				    setChanged();
+					notifyObservers(line);
+					connection.close();
+			    }
+			} catch (IOException e) {
+				e.printStackTrace();
+			} finally {
+				// sockets are closed when complete.
+				try {
+					if (connection != null)
+						connection.close();
+				} catch (IOException e) {
+				}
+			}
+		}
+		LOG.warn("Thread " + Thread.currentThread().getName() + " exited");
+	}
+	
+	@Override
+	protected void finalize() throws Throwable {
+		server.close();
+		LOG.info("Thread " + Thread.currentThread().getName() + " shutting down.");
+		super.finalize();
+	}
+	
+}
---
+++ src/main/java/com/nitinsurana/bitcoinlitecoin/rpcconnector/events/WalletListener.java
@@ -0,0 +1,54 @@
+package com.nitinsurana.bitcoinlitecoin.rpcconnector.events;
+
+import com.nitinsurana.bitcoinlitecoin.rpcconnector.CryptoCurrencyRPC;
+import com.nitinsurana.bitcoinlitecoin.rpcconnector.exception.BitcoindException;
+import com.nitinsurana.bitcoinlitecoin.rpcconnector.pojo.Transaction;
+
+import java.io.IOException;
+import java.util.Observable;
+import java.util.Observer;
+
+
+public class WalletListener extends Observable implements Observer {
+
+	final private Observable walletListener;
+	final private CryptoCurrencyRPC client;
+	public Thread listener = null;
+
+	public WalletListener(final CryptoCurrencyRPC client, int port) throws IOException {
+		walletListener = new BitcoinDListener(port);
+		this.client = client;
+	}
+
+	@Override
+	public synchronized void addObserver(Observer o) {
+		if (listener == null) {
+			walletListener.addObserver(this);
+			listener = new Thread((Runnable) walletListener, "walletListener");
+			listener.start();
+		}
+		super.addObserver(o);
+	}
+
+	@Override
+	public void update(Observable o, Object arg) {
+		final String value = ((String) arg).trim();
+		new Thread() {
+			public void run() {
+				try {
+					Transaction tx = client.getTransaction(value);
+					setChanged();
+					notifyObservers(tx);
+				} catch (BitcoindException e) {
+					e.printStackTrace();
+				}
+
+			}
+		}.start();
+	}
+	
+	public void stop(){
+		listener.interrupt();
+	}
+
+}
