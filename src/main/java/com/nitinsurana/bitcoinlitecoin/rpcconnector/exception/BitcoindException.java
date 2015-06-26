29cf711c3a79949ba1505c3302b85f2e7610b3f5
Romantsov <dm.romantsov@gmail.com>
2015-06-26 14:57:22 +0300

    Change BitcoindException extending to RuntimeException.

--- src/main/java/com/nitinsurana/bitcoinlitecoin/rpcconnector/exception/BitcoindException.java
+++ src/main/java/com/nitinsurana/bitcoinlitecoin/rpcconnector/exception/BitcoindException.java
@@ -3,7 +3,7 @@ package com.nitinsurana.bitcoinlitecoin.rpcconnector.exception;
 /**
  * Created by d.romantsov on 22.05.2015.
  */
-public class BitcoindException extends Exception {
+public class BitcoindException extends RuntimeException {
 
     public BitcoindException(String message) {
         super(message);
