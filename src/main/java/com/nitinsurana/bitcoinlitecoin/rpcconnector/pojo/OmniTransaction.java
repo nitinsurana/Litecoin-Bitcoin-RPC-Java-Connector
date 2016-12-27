70b909e1855533fb788510a0755230fa59e35dea
DmRomantsov <dm.romantsov@gmail.com>
2016-12-27 17:54:21 +0200

    property id in tx.

--- src/main/java/com/nitinsurana/bitcoinlitecoin/rpcconnector/pojo/OmniTransaction.java
+++ src/main/java/com/nitinsurana/bitcoinlitecoin/rpcconnector/pojo/OmniTransaction.java
@@ -6,6 +6,7 @@ package com.nitinsurana.bitcoinlitecoin.rpcconnector.pojo;
 public class OmniTransaction extends Transaction {
     private String sendingaddress;
     private String referenceaddress;
+    private Long propertyid;
 
     public String getReferenceaddress() {
         return referenceaddress;
@@ -21,5 +22,13 @@ public class OmniTransaction extends Transaction {
 
     public void setSendingaddress(String sendingaddress) {
         this.sendingaddress = sendingaddress;
+    }
+
+    public Long getPropertyid() {
+        return propertyid;
+    }
+
+    public void setPropertyid(Long propertyid) {
+        this.propertyid = propertyid;
     }
 }
